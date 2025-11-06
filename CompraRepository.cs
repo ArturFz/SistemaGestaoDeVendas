a9a866381170db76c41047700a7bdd889371d771
guoliveira0 <luizgustavo160206@gmail.com>
2025-11-06 08:56:46 -0300

    Criei a classe CompraRepository

---
+++ CompraRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabalho1_ProgVis;
+
+namespace Trabalho_TCD
+{
+    internal class CompraRepository
+    {
+        public static void SaveOrUpdate (Compra compra)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    if (compra.Id == 0)
+                    {
+                        dbContext.Compras.Add(compra);
+                    }
+                    else
+                    {
+                        dbContext.Entry(compra).State
+                            = EntityState.Modified;
+                    }
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static List<Compra> FindAll()
+        {
+            try
+            {
+                using(Repository dbContext = new Repository())
+                {
+                    return dbContext.Compras.ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static Compra FindById (UInt64 id)
+        {
+            try
+            {
+                using (Repository dbContext = new Repository())
+                {
+                    return dbContext.Compras.Find(id);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        
+    }
+}
