8832a889bf02d54b0df8668124e2b3702c4ad866
Artur Felipe dos Santos Gonçalves <artirfelipe363@gmail.com>
2025-11-30 15:58:40 -0300

    .

--- Sistema.cs
+++ Sistema.cs
@@ -179,10 +179,5 @@ namespace Trabalho1_ProgVis
 
             relatorioProdutos.Show();
         }
-
-        public static Usuario? GetUsuarioLogado()
-        {
-            return _usuario;
-        }
     }
 }
